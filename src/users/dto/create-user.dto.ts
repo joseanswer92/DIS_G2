25c9842e25fbc7a231e70ca171313b4af7f5f9f3
joseanswer92 <joseanswer@gmail.com>
2026-02-19 21:41:36 -0600

    feat: se agregaron los endpoints de usuario

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ required: true, example: 1, description: 'ID del tenant' })
+  @IsInt()
+  @Min(1)
+  tenantId: number;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  create(dto: CreateUserDto) {
+    return this.prisma.user.create({
+      data: {
+        tenantId: dto.tenantId,
+        // Si querés crear profile automáticamente:
+        // profile: { create: {} },
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany({ include: { profile: true, tenant: true } });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUnique({
+      where: { id },
+      include: { profile: true, tenant: true },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(id: number, dto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: {
+        ...(dto.tenantId ? { tenantId: dto.tenantId } : {}),
+      },
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.prisma.user.delete({ where: { id } });
   }
 }
